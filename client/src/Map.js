517e80f11a88fd4c55f45bcc92aac78a3d7b2270
Elizabeth Vasquez <vasquezelizabeth03@gmail.com>
2019-10-01 17:17:37 -0500

    added side div

--- client/src/Map.js
+++ client/src/Map.js
@@ -2,56 +2,27 @@ import React,{useState} from 'react'
 import { Map, Marker, InfoWindow, GoogleApiWrapper } from 'google-maps-react';
 import { useLayoutEffect } from 'react';
 import { useEffect } from 'react';
-const API_KEY = 'AIzaSyBCTH0Dn95-mjPC-ZTMMGxbqPLrrl-DFw0'
+const API_KEY = ''
 
+
+//need to change 
 const mapStyles={
-    height: '100vh',
-    width:'100%'
+    height: '100%', 
+    width:'100%',
+    position: 'absolute'
 }
 
-// const location={
-//     city: 'Richmond',
-//     image: null,
-//     locationName:'Michael Messina',
-//     owner:"tz0IZXBYLRUlP6dO37hf6XfUUI12",
-//     poolSize:20000,
-//     poolType:"chlorine",
-//     pumpHP: 2,
-//     pumpStyle:'single speed',
-//     zip:'77406'
-// }
 
-// const customerDoc ={
-//     accessCode: '123',
-//     billPlanId: 'O6VCBI0ZRS6FFXm3AUCH',
-//     billingAddr: '1334 Brittmore Rd',
-//     billingAddr2: null,
-//     billingCity: 'Houston',
-//     billingSameAddr: true,
-//     billingState: 'TX',
-//     billingZip: '77043',
-//     email: 'demo@gmail.com',
-//     firstName: 'John',
-//     homePhone:'1234567',
-//     lastName: 'Doe',
-//     locationId: 'Em6eaCya7U0KVHpKFkEa',
-//     mobile: '000-000-0000',
-//     note: 'My pool is clean',
-//     receiveEmail: true,
-//     receiveText: true,
-//     techId: 'rCEUvSwBXqhaB1MJDdQvRM74MFH3',
-//     work: '000-000-0000'
-// }
-
-
-
+//need to integrate this into dashboard. 
 
 export const MapTest = (props) =>{
-    let [markerInfo, setMarkerInfo] = useState({
-        showingInfoWindow: false,
-        activeMarker:{},             
-    })
+    // let [markerInfo, setMarkerInfo] = useState({
+    //     showingInfoWindow: false,
+    //     activeMarker:{},             
+    // })
 
+
+    //use geocoding for the addresses to convert it to lat/long?
     let [locations, setLocations]=useState([
         {latitude:29.794940, longitude:-95.569930}, 
         {latitude:29.784013, longitude:-95.651611},
@@ -62,19 +33,18 @@ export const MapTest = (props) =>{
     let [animation, setAnimation] = useState(null)
     
 
-    console.log(locations)
 
-    //args: 
+
+    //Need to present info on side div when marker clicked
     const onMarkerClick=(props,marker,e)=>{
         if(marker.animation !== null){
             marker.setAnimation(null)
         }else{
             marker.setAnimation(1)
-        }
-        
+        }   
     }
 
-
+    //Need to change color depending on driver 
     const createMarkers=()=>{
         return locations.map(loc=>{
             return(
@@ -87,24 +57,35 @@ export const MapTest = (props) =>{
     }
    
     return(
-        <Map
-        google={props.google}
-        zoom={8}
-        style={mapStyles}
-        initialCenter={{lat: 29.794940, lng: -95.569930}}
-        >
+        <div class="container">
+            <Map
+            google={props.google}
+            zoom={8}
+            style={mapStyles}
+            initialCenter={{lat: 29.794940, lng: -95.569930}}
+            >
+            
         
-       
-        {createMarkers()}
+            {/* {createMarkers()} */}
 
-        <InfoWindow 
-            marker={markerInfo.activeMarker}
-            visible={markerInfo.showingInfoWindow}>
-            <div>
-                <h1>test</h1>
+            {/* <InfoWindow 
+                marker={markerInfo.activeMarker}
+                visible={markerInfo.showingInfoWindow}>
+                <div>
+                    <h1>test</h1>
+                </div>
+            </InfoWindow> */}
+            </Map>
+            <div className="customer-div">
+                <h3>Customer Info</h3>
+                <div className="customer-info">
+                    <p>Name:</p>
+                    <p>Address</p>
+                    <p>Live Data:</p>
+                    <div class="live-data"></div>
+                </div>
             </div>
-        </InfoWindow>
-        </Map>
+        </div>
     )
     
 }
