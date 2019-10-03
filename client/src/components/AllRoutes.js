ca37112d3831d81e28eece2970ae96712d6a3f69
Elizabeth Vasquez <vasquezelizabeth03@gmail.com>
2019-10-03 12:17:06 -0500

    refactoring--changed to hooks

--- client/src/components/AllRoutes.js
+++ client/src/components/AllRoutes.js
@@ -13,16 +13,12 @@ const Container = styled.div`
 
 const initialStops = {
     stops: {
-        'stop-1': {id: 'stop-1', content: 'S1', latitude:29.794940, longitude: -95.569930},
-        'stop-2': {id: 'stop-2', content: 'S2', latitude:29.794940, longitude: -95.569930},
-        'stop-3': {id: 'stop-3', content: 'S3', latitude:29.794940, longitude: -95.569930},
-        'stop-4': {id: 'stop-4', content: 'S4', latitude:29.794940, longitude: -95.569930},
-        'stop-5': {id: 'stop-5', content: 'S5', latitude:29.794940, longitude: -95.569930},
-        'stop-1': {id: 'stop-1', content: '',},
-        'stop-2': {id: 'stop-2', content: ''},
-        'stop-3': {id: 'stop-3', content: ''},
-        'stop-4': {id: 'stop-4', content: ''},
-        'stop-5': {id: 'stop-5', content: ''},
+        'stop-1': {id: 'stop-1', content: '', latitude:29.794940, longitude: -95.569930},
+        'stop-2': {id: 'stop-2', content: '', latitude:29.794940, longitude: -95.569930},
+        'stop-3': {id: 'stop-3', content: '', latitude:29.794940, longitude: -95.569930},
+        'stop-4': {id: 'stop-4', content: '', latitude:29.794940, longitude: -95.569930},
+        'stop-5': {id: 'stop-5', content: '', latitude:29.794940, longitude: -95.569930},
+
     },
     driverRoutes: {
         'driverRoute-1': {
@@ -46,108 +42,103 @@ const initialStops = {
 };
 
 
-class AllRoutes extends React.Component {
-    state = initialStops;
-
-
-    //  onDragStart =() => {
-    //      document.body.style.color = 'orange'
-    //      document.body.style.transition = 'background-color 0.2s ease';
-    //  }
-
-     onDragUpdate = update => {
-         const {destination} = update;
-         const opacity = destination ? destination.index / Object.keys(this.state.stops).length : 0;
-         document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity})`
-         }
-
-    onDragEnd = result => {
-        document.body.style.color = 'inherit';
-        document.body.style.backgroundColor = 'inherit';
-
-        const { destination, source, draggableId } = result;
-        
-        if(!destination){
-            return;
-        }
-        //see if the location of the draggable changed
-        if(
-            destination.droppableId === source.droppableId &&
-            destination.index === source.index
-        ){
-            return;
-        }
-        const start = this.state.driverRoutes[source.droppableId];
-        const finish = this.state.driverRoutes[destination.droppableId];
-
-        if(start === finish){
-            const newStopIds = Array.from(start.stopIds);
-            newStopIds.splice(source.index, 1);
-            newStopIds.splice(destination.index, 0, draggableId);
-
-            const newDriverRoute = {
-                ...start,
-                stopIds: newStopIds,
-            };
-            const newState = {
-                ...this.state,
-                driverRoutes: {
-                    ...this.state.driverRoutes,
-                    [newDriverRoute.id]: newDriverRoute
-                }
-            }
-            this.setState(newState);   
-            return;         
-        }
-
-        //moving from one list to another
-        const startStopIds = Array.from(start.stopIds);
-        startStopIds.splice(source.index, 1);
-        const newStart = {
-            ...start,
-            stopIds: startStopIds,
-        };
-        const finishStopIds = Array.from(finish.stopIds);
-        finishStopIds.splice(destination.index, 0, draggableId);
-        const newFinish = {
-            ...finish,
-            stopIds: finishStopIds,
-        }
-
-        const newState = {
-            ...this.state,
-            driverRoutes: {
-                ...this.state.driverRoutes,
-                [newStart.id]: newStart,
-                [newFinish.id]: newFinish,
-            },
-        }
-        this.setState(newState)
-
-
-    };
-
-    render() {
-        return (
-            <div>
-                <Title>Technicians</Title>
-
-            <DragDropContext 
-            onDragStart={this.onDragStart}
-            onDragUpdate={this.onDragUpdate}
-            onDragEnd={this.onDragEnd} >
-                <Container>
-            {this.state.driverRouteOrder.map((driverRouteId) => {
-            const driverRoute = this.state.driverRoutes[driverRouteId];
-            const stops = driverRoute.stopIds.map(stopId => this.state.stops[stopId]);
-
-            return <DriverRoute key={driverRoute.id} driverRoute={driverRoute} stops={stops} />
-        })}
-        </Container>
-        </DragDropContext>
-        </div>
-        );
+const AllRoutes = () =>{
+    let [routeStops, setRouteStops] = useState(initialStops)
+    const onDragStart =() => {
+        document.body.style.color = 'orange'
+        document.body.style.transition = 'background-color 0.2s ease';
     }
-}
+
+    const onDragUpdate = update => {
+        const {destination} = update; 
+        const opacity = destination ? destination.index / Object.keys(routeStops.stops).length : 0;
+        document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity})`
+        }
+
+   const onDragEnd = result => {
+       document.body.style.color = 'inherit';
+       document.body.style.backgroundColor = 'inherit';
+
+       const { destination, source, draggableId } = result;
+       
+       if(!destination){
+           return;
+       }
+       //see if the location of the draggable changed
+       if(
+           destination.droppableId === source.droppableId &&
+           destination.index === source.index
+       ){
+           return;
+       }
+       const start = routeStops.driverRoutes[source.droppableId];
+       const finish = routeStops.driverRoutes[destination.droppableId];
+
+       if(start === finish){
+           const newStopIds = Array.from(start.stopIds);
+           newStopIds.splice(source.index, 1);
+           newStopIds.splice(destination.index, 0, draggableId);
+
+
+           const newDriverRoute = {
+               ...start,
+               stopIds: newStopIds,
+           };
+           const newState = {
+               ...routeStops,
+               driverRoutes: {
+                   ...routeStops.driverRoutes,
+                   [newDriverRoute.id]: newDriverRoute
+               }
+           }
+           setRouteStops(newState);   
+           return;         
+       }
+
+       //moving from one list to another
+       const startStopIds = Array.from(start.stopIds);
+       startStopIds.splice(source.index, 1);
+       const newStart = {
+           ...start,
+           stopIds: startStopIds,
+       };
+       const finishStopIds = Array.from(finish.stopIds);
+       finishStopIds.splice(destination.index, 0, draggableId);
+       const newFinish = {
+           ...finish,
+           stopIds: finishStopIds,
+       }
+
+       const newState = {
+           ...routeStops,
+           driverRoutes: {
+               ...routeStops.driverRoutes,
+               [newStart.id]: newStart,
+               [newFinish.id]: newFinish,
+           },
+       }
+       setRouteStops(newState)
+
+
+   };
+
+
+       return (
+           <DragDropContext 
+           onDragStart={onDragStart}
+           onDragUpdate={onDragUpdate}
+           onDragEnd={onDragEnd} >
+               <Container>
+           {routeStops.driverRouteOrder.map((driverRouteId) => {
+           const driverRoute = routeStops.driverRoutes[driverRouteId];
+           const stops = driverRoute.stopIds.map(stopId => routeStops.stops[stopId]);
+
+           return <DriverRoute key={driverRoute.id} driverRoute={driverRoute} stops={stops} />
+       })}
+       </Container>
+       </DragDropContext>
+       );
+   }
+
 
 export default AllRoutes
